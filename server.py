575884ecea5469431b49d1f9fbdbc6bfbd5f7e04
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-09-02 15:33:32 +0500

    Implement def archive

--- server.py
+++ server.py
@@ -1,9 +1,26 @@
 from aiohttp import web
 import aiofiles
+import asyncio
+import time
+
+CHUNK_SIZE = 100 * 1024
 
 
 async def archive(request):
-    raise NotImplementedError
+    archive_hash = request.match_info.get('archive_hash', '7kna')
+    response = web.StreamResponse()
+    response.headers['Content-Disposition'] = f'attachment; filename="{archive_hash}.zip"'
+    response.headers['Content-Type'] = 'application/zip, application/octet-stream'
+    await response.prepare(request)
+    process = await asyncio.create_subprocess_shell(
+        f'zip -j -r - test_photos/{archive_hash}',
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
+    )
+    while not process.stdout.at_eof():
+        chunk = await process.stdout.read(CHUNK_SIZE)
+        await response.write(chunk)
+    return response
 
 
 async def handle_index_page(request):
